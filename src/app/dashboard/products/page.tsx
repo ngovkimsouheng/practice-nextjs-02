6994c0be52070401c0e95ee7e1ae8e5d206b7b2b
ngovkimsouheng <kokocorona9@gmail.com>
2026-06-03 09:37:18 +0700

    add meta data for product page

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,15 @@
 
 import React from 'react'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Products | Next Route",
+  description: "Manage and view all your products",
+  openGraph: {
+    title: "Products | Next Route",
+    description: "Manage and view all your products",
+  },
+}
 
 export default function ProductsInDashboard() {
   return (
