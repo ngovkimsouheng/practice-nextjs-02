520e596fd19500faf2e884db042208ad410bccfd
ngovkimsouheng <kokocorona9@gmail.com>
2026-06-03 09:49:53 +0700

    add metadata for product detail

--- src/app/dashboard/products/[...uuid]/page.tsx
+++ src/app/dashboard/products/[...uuid]/page.tsx
@@ -1,29 +1,24 @@
-
-import { Metadata } from 'next'
-
+import { Metadata } from "next";
 
 export async function generateMetadata({
   params,
 }: {
-  params: { uuid: string }
+  params: { uuid: string };
 }): Promise<Metadata> {
+  const res = await fetch(`https://ishop.cheat.casa/api/v1https://ishop.cheat.casa/api/v1/products/${params.uuid}`);
+  const post = await res.json();
+
   return {
-    title: `Product ${params.uuid} | Next Route`,
-    description: `View details for product ${params.uuid}`,
+    title: `Product ${post?.uuid} | Next Route`,
+    description: `View details for product ${post?.uuid}`,
     openGraph: {
-      title: `Product ${params.uuid} | Next Route`,
-      description: `View details for product ${params.uuid}`,
+    
+      title: `Product ${post?.uuid} | Next Route`,
+      description: `View details for product ${post?.uuid}`,
     },
-  }
+  };
 }
 
-export default function Page({
- params,
-}:{
- params:{uuid:string}
-}){
-
- return (
-    <div>{params.uuid}</div>
- )
+export default function Page({ params }: { params: { uuid: string } }) {
+  return <div>{params.uuid}</div>;
 }
