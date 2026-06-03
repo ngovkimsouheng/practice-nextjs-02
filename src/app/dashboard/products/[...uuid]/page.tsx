67461e5fedc1c1f9f8bd36eb1a145a0e4719744f
ngovkimsouheng <kokocorona9@gmail.com>
2026-06-03 09:52:05 +0700

    add image

--- src/app/dashboard/products/[...uuid]/page.tsx
+++ src/app/dashboard/products/[...uuid]/page.tsx
@@ -5,16 +5,18 @@ export async function generateMetadata({
 }: {
   params: { uuid: string };
 }): Promise<Metadata> {
-  const res = await fetch(`https://ishop.cheat.casa/api/v1https://ishop.cheat.casa/api/v1/products/${params.uuid}`);
+  const res = await fetch(
+    `https://ishop.cheat.casa/api/v1https://ishop.cheat.casa/api/v1/products/${params.uuid}`,
+  );
   const post = await res.json();
 
   return {
     title: `Product ${post?.uuid} | Next Route`,
     description: `View details for product ${post?.uuid}`,
     openGraph: {
-    
-      title: `Product ${post?.uuid} | Next Route`,
-      description: `View details for product ${post?.uuid}`,
+      images: post?.thumbnail,
+      title: `Product ${post?.name} | Next Route`,
+      description: `View details for product ${post?.description}`,
     },
   };
 }
